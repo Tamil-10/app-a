efdf708e26a2853bf84b430e06b07f0fdc4c5c73
Tamil Selvan <tamil@Tamils-MacBook-Pro.local>
2026-03-25 23:01:12 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/app-a/',
   server: {
     port: 5173,
   },
-  base: '/'
 })
